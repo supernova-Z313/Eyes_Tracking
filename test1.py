a28dd639bf08196ebc09d891d24ead4ffd95cdfa
Supernova <supernova313.313z@gmail.com>
2023-12-25 05:06:42 +0330

    crop eyes and set place in window

--- test1.py
+++ test1.py
@@ -15,46 +15,58 @@ def detecting(img, cascade_type):
 
 
 # =============================================================
-def resizer(name, x, y):
-	# 720 * 1280 : ahmad
-	cv2.namedWindow(name, cv2.WINDOW_NORMAL) 
-	cv2.resizeWindow(name, x, y)
+def resizer(name, x, y, flag=cv2.WINDOW_NORMAL):
+	cv2.namedWindow(name, flag)
+	if flag != cv2.WINDOW_AUTOSIZE:
+		cv2.resizeWindow(name, x, y)
 
 
 # =============================================================
 def clearify_selection_with_face(face_cord, eyes_cord):
-	# image or so little or so big or under the half of face or not in face deleted
+	"""
+		image or so little or so big or 
+		under the half of face or not in face deleted
+		[ ] overlap or more than one 	
+	"""
 	face_end_x = face_cord[0][0] + face_cord[0][2]
 	face_end_y = face_cord[0][1] + face_cord[0][3]
 	face_eara = face_cord[0][2] * face_cord[0][3]
 	final = []
-	for i in eyes_cord:
+	# print(face_cord[0])
+	# print("hello -------------------")
+	for ind, i in enumerate(eyes_cord):
+		if ind >= 2:
+			break
+
 		eara = i[2]*i[3]
 		end_x = i[0] + i[2]
 		end_y = i[1] + i[3]
 
 		# ------------------------------------ not in face
-		if face_cord[0][0] > start_x:
+		if face_cord[0][0] > i[0]:
 			continue
-		if face_cord[0][1] > start_y:
+		if face_cord[0][1] > i[1]:
 			continue
 		if face_end_x < end_x:
 			continue
 		if face_end_y < end_y:
 			continue
-
 		# ------------------------------------ under the 65%
 		if i[1] > (face_cord[0][1] + (face_cord[0][3]*65/100)):
 			continue
-
 		# ------------------------------------ so big or little
-		if not(2.335 <= (eara*100)/(face_eara) <= 4.111):
+		# print(i)
+		# print()
+		if not(2.546 <= (eara*100)/(face_eara) <= 10.458):
 			continue
+		# ------------------------------------ so big or little
+		final.append(i)
+	return final
 
 
-
-		
-
+# =============================================================
+def clearify_selection_with_no_face():
+	pass
 
 # =============================================================
 if __name__=="__main__":
@@ -62,7 +74,15 @@ if __name__=="__main__":
 	eye_cascade = cv2.CascadeClassifier('./data/haarcascade_eye.xml')
 
 	cap = cv2.VideoCapture("./media/ahmad.mp4")
+	# 720 * 1280 : ahmad : 360, 640
+	# 1080 * 1920 : rastegar : 540, 960
 	resizer("video", 360, 640)
+	resizer("left", 300, 320) # cv2.WINDOW_AUTOSIZE
+	resizer("right", 300, 320) # cv2.WINDOW_AUTOSIZE
+	cv2.moveWindow("video", 900, 10)
+	cv2.moveWindow("left", 100, 150)
+	cv2.moveWindow("right", 450, 150)
+
 	
 	while True:
 		ret, frame = cap.read()
@@ -71,19 +91,28 @@ if __name__=="__main__":
 
 		frame, face_cord = detecting(frame, face_cascade)
 		frame, eyes_cord = detecting(frame, eye_cascade)
-		draw_rec(frame, eyes_cord, (0, 255, 0))
+		# draw_rec(frame, eyes_cord, (0, 255, 0))
 		draw_rec(frame, face_cord, (0, 0, 255))
+		clean1 = []
 
-		# better to clearify all and with face only just with face
 		if len(face_cord):
-			clearify_selection_with_face(face_cord, eyes_cord)
-		else:
-			# overlap or more than one 
-			clearify_selection_with_mo_face()
+			clean1 = clearify_selection_with_face(face_cord, eyes_cord)
+			draw_rec(frame, clean1, (255, 0, 0))
+		if len(clean1) == 2:
+			if clean1[0][0] < clean1[1][0]:
+				l = clean1[0]
+				r = clean1[1]
+			else:
+				l = clean1[1]
+				r = clean1[0]
 
-		# roi = frame[]
+			left = frame[l[1]: l[1]+l[3], l[0]: l[0]+l[2]]
+			right = frame[r[1]: r[1]+r[3], r[0]: r[0]+r[2]]
+			cv2.imshow("left", left)
+			cv2.imshow("right", right)
+
 		cv2.imshow("video", frame)
-		key = cv2.waitKey(1000)
+		key = cv2.waitKey(30)
 		if key == ord('q'): # quit
 			break
 		elif key == ord('s'): # stop
