2f9be7b234f1aaafcfd5c7e57bd211c04b2b9317
Supernova <supernova313.313z@gmail.com>
2023-12-27 02:36:45 +0330

    first final fase

--- test1.py
+++ test1.py
@@ -22,16 +22,13 @@ def resizer(name, x, y, flag=cv2.WINDOW_NORMAL):
 # =============================================================
 def clearify_selection_with_face(face_cord, eyes_cord):
 	"""
-		image or so little or so big or 
-		under the half of face or not in face deleted
-		[ ] overlap or more than one 	
+		image or so little or so big or under the half of face or not in face deleted
+		TODO : [ ] overlap or more than one 	
 	"""
 	face_end_x = face_cord[0][0] + face_cord[0][2]
 	face_end_y = face_cord[0][1] + face_cord[0][3]
 	face_eara = face_cord[0][2] * face_cord[0][3]
 	final = []
-	# print(face_cord[0])
-	# print("hello -------------------")
 	for ind, i in enumerate(eyes_cord):
 		if ind >= 2:
 			break
@@ -51,8 +48,6 @@ def clearify_selection_with_face(face_cord, eyes_cord):
 		if i[1] > (face_cord[0][1] + (face_cord[0][3]*65/100)):
 			continue
 		# ------------------------------------ so big or little
-		# print(i)
-		# print()
 		if not(2.546 <= (eara*100)/(face_eara) <= 10.458):
 			continue
 		# ------------------------------------ so big or little
@@ -70,18 +65,50 @@ def find_best_contours(temp, temp_threshold):
 	"""
 	contours, _ = cv2.findContours(temp_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	# contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-	rect = 2.3
+	rect = 2.1 # 2.3
+	little = 1.5
+	big = 10
+	edge_percent = 22
+	o_edge_percent = 100 - edge_percent
+	f_h, f_w = temp.shape
+	ans = []
+	# print("\n---------------")
 	for ind, cnt in enumerate(contours, start=0):
-		(c_x, x_y, c_w, c_h) = cv2.boundingRect(cnt)
+		(c_x, c_y, c_w, c_h) = cv2.boundingRect(cnt)
 		# ----------------------- rectangle
 		if (c_w/c_h > rect) or (c_h/c_w > rect):
 			continue
-		# ----------------------- 
-
-		# draw_rec(temp, [[c_x, x_y, c_w, c_h]], (255, 0, 0), 1)
+		# ----------------------- little or big
+		if not(little < (c_w*c_h*100)/(f_h*f_w) < big):
+			continue
+		# ----------------------- in the edge
+		# (c_x < f_w*edge_percent/100) or (c_x+c_w > f_w*o_edge_percent/100)
+		if (c_y < f_h*edge_percent/100) or (c_y+c_h > f_h*o_edge_percent/100):
+			continue
+		# ----------------------- only 3 of biggest (needed to sort)
+		# if ind > 2:
+		# 	break
+		# -----------------------
+		ans.append([c_x, c_y, c_w, c_h])
+		# draw_rec(temp, [[c_x, c_y, c_w, c_h]], (255, 0, 0), 1)
 		# cv2.drawContours(left, [cnt], -1, (0, 0, 255), 2)
-		if ind > 1:
-			break
+	return ans
+
+# =============================================================
+def find_center_point(rectangles):
+	mids = {}
+	for i in rectangles:
+		mids[(i[0]+(i[2]/2), i[1]+(i[3]/2))] = i[2]*i[3]
+	temp_x = 0
+	temp_y = 0
+	erea = 0
+	for i in mids:
+		erea += mids[i]
+		temp_x += i[0]*mids[i]
+		temp_y += i[1]*mids[i]
+	middle_x = temp_x/erea
+	middle_y = temp_y/erea
+	return (middle_x, middle_y)
 
 # =============================================================
 def left_right_selection(eyes):
@@ -102,19 +129,41 @@ def eyes_frame_seter(frame, lor):
 	return temp, temp_threshold
 
 # =============================================================
+def directions(m_x, m_y, shape, words):
+	m_x, m_y = int(m_x), int(m_y)
+	vertical_top_border = 3.4
+	vertical_down_border = 2.9
+	horizontal_right_border = 9.6
+	horizontal_left_border = 8.9
+	ans = None
+	# ------------------------ first horizontal second vertical
+	if m_x < (shape/2)-(shape*horizontal_left_border/100):
+		ans = "Left"
+	elif m_x > (shape/2)+(shape*horizontal_right_border/100):
+		ans = "Right"
+	elif m_y < (shape/2)-(shape*vertical_top_border/100):
+		ans = "Top"
+	elif m_y > (shape/2)+(shape*vertical_down_border/100):
+		ans = "Down"
+	else:
+		ans = "Center"
+	# print(words, ":", ans)
+	return ans
+
+# =============================================================
 def set_name_and_position():
 	# 720 * 1280 : ahmad : 360, 640
 	# 1080 * 1920 : rastegar : 540, 960
 	resizer("video", 360, 640)
 	resizer("left", 200, 250) # cv2.WINDOW_AUTOSIZE
 	resizer("right", 200, 250) # cv2.WINDOW_AUTOSIZE
-	resizer("left_threshold", 200, 250)
-	resizer("right_threshold", 200, 250)
+	# resizer("left_threshold", 200, 250)
+	# resizer("right_threshold", 200, 250)
 	cv2.moveWindow("video", 900, 10)
 	cv2.moveWindow("left", 150, 100)
 	cv2.moveWindow("right", 400, 100)
-	cv2.moveWindow("left_threshold", 150, 450)
-	cv2.moveWindow("right_threshold", 400, 450)	
+	# cv2.moveWindow("left_threshold", 150, 450)
+	# cv2.moveWindow("right_threshold", 400, 450)
 
 # =============================================================
 if __name__=="__main__":
@@ -145,16 +194,38 @@ if __name__=="__main__":
 			left, left_threshold = eyes_frame_seter(frame, l)
 			right, right_threshold = eyes_frame_seter(frame, r)
 			
-			find_best_contours(left, left_threshold)
-			find_best_contours(right, right_threshold)
+			left_ans = find_best_contours(left, left_threshold)
+			right_ans = find_best_contours(right, right_threshold)
 
-			cv2.imshow("left_threshold", left_threshold)
-			cv2.imshow("right_threshold", right_threshold)
+			if len(left_ans) and len(right_ans):
+				l_m_x, l_m_y = find_center_point(left_ans)
+				r_m_x, r_m_y = find_center_point(right_ans)
+
+				l_ans = directions(l_m_x, l_m_y, left.shape[0], "left eye")
+				# print(left.shape)
+				# print(l_m_x, l_m_y)
+				r_ans = directions(r_m_x, r_m_y, right.shape[0], "right eye")
+				# print(right.shape)
+				# print(r_m_x, r_m_y)
+				# print("------------------")
+
+				# ---------------------------- check some last direction ...
+				# shift register 5tayi
+				if l_ans == r_ans:
+					print("Direction is: ", l_ans)
+				else:
+					print("Direction is: Center")
+
+				cv2.circle(left, (int(l_m_x), int(l_m_y)), 4, (0, 255, 0), 4)
+				cv2.circle(right, (int(r_m_x), int(r_m_y)), 4, (0, 255, 0), 4)
+
+			# cv2.imshow("left_threshold", left_threshold)
+			# cv2.imshow("right_threshold", right_threshold)
 			cv2.imshow("left", left)
 			cv2.imshow("right", right)
 
 		cv2.imshow("video", frame)
-		key = cv2.waitKey(30)
+		key = cv2.waitKey(20)
 		if key == ord('q'): # quit
 			break
 		elif key == ord('s'): # stop
